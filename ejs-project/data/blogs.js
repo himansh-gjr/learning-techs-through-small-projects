aa80ad673ce068f711398f48a32ddebf0e541f84
himansh-gjr <himanshugjr1810@gmail.com>
2021-09-08 18:17:29 +0530

    ejs project

---
+++ ejs-project/data/blogs.js
@@ -0,0 +1,7 @@
+const BLOGS = [
+    {id:1,title:'first blog',data:'para of the first block with some lorem ispem'},
+    {id:2,title:'second blog',data:'para of the second block with some lorem ispem'},
+    {id:3,title:'third blog',data:'para of the third block with some lorem ispem'},
+]
+
+module.exports  = {BLOGS}
